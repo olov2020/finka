d52a0150976f2457c452a0cc133fc8fca283c649
olov2020 <vladimir.8624@mail.ru>
2025-01-24 15:47:59 +0300

    start working on account screens. create screen for account, registration and login. write api for registration and login

---
+++ finka_front/screens/home/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home page</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
+}
+
+export default HomeScreen;
