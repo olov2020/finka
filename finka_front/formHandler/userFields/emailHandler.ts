00a88a21a11de9f8b9d0caa14f90a29626f67253
olov2020 <vladimir.8624@mail.ru>
2025-01-31 15:22:22 +0300

    add required parameter to validation of input fields

--- finka_front/formHandler/userFields/emailHandler.ts
+++ finka_front/formHandler/userFields/emailHandler.ts
@@ -1,5 +1,12 @@
 // @ts-ignore
-const emailHandler = ({value}) => {
+const emailHandler = ({value, required}) => {
+    if (value.length === 0 && !required) {
+        return 'success';
+    }
+
+    if (value.length === 0 && required) {
+        return 'Данное поле не может быть пустым';
+    }
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     return emailRegex.test(value) ? 'success' : 'Неверный формат ввода почты';
