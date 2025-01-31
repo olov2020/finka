4de884e7af3f9bf8e7d0dc155f99a5b4e3e4bba4
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-01-31 23:02:26 +0300

    changed urls.py

--- finka_back/finka/urls.py
+++ finka_back/finka/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('login', views.login),
 ]
