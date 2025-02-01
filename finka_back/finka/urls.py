0e4260230c9155c28c42bc5a8cfc4b914721e2ed
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-02-01 13:00:13 +0300

    updated urls.py

--- finka_back/finka/urls.py
+++ finka_back/finka/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     re_path('login', views.login),
     re_path('signup', views.signup),
     re_path('test_token', views.test_token),
+    re_path('change_username', views.change_username),
+    re_path('change_email', views.change_email),
+    re_path('change_password', views.change_password),
 ]
