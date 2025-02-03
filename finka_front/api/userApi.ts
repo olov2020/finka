385d1e1865e75bd6d7a50133a59db257c75f51d0
olov2020 <vladimir.8624@mail.ru>
2025-02-03 14:18:04 +0300

    change urls to match backend

--- finka_front/api/userApi.ts
+++ finka_front/api/userApi.ts
@@ -1,16 +1,19 @@
 import {$host} from "./axiosApi";
 
 export const userRegistrationApi = async (email: string, password: string) => {
-    return await $host.post(`/signUp/`, {email, password})
+    return await $host.post(`/signup/`, {email, password})
         .then((response) => {
             return response.data;
         })
-        .catch((error: { message: '' }) => {
-            return new Error(error.message);
+        .catch((error) => {
+            return new Error(error.data.message);
         })
 };
 
 export const userLoginApi = async (email: string, password: string) => {
-    const response = await $host.post(`/signIn/`, {email, password});
+    const response = await $host.post(`/login/`, {email, password});
+
+    localStorage.setItem("access_token", response.data.access_token);
+    localStorage.setItem("refresh_token", response.data.refresh_token);
     return response.data;
 };
