3dc9edfb27552e3ed1bef796afaab5896bdb0a9f
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-02-05 19:10:30 +0300

    updated urls for jwt

--- finka_back/finka/urls.py
+++ finka_back/finka/urls.py
@@ -1,6 +1,9 @@
-from django.urls import re_path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # update token
+    path('api/register/', RegistrationView.as_view(), name='register'), # registration
 ]
