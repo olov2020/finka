06141eceac2251df69dc028020b61e3b21f5c19b
olov2020 <vladimir.8624@mail.ru>
2025-02-10 11:59:23 +0300

    change some params in http requests for signup and login functions. add interceptor for authHost to use access_token in headers of authorized requests

--- finka_front/api/axiosApi.ts
+++ finka_front/api/axiosApi.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
 
 export const $host = axios.create({
-    baseURL: 'http://127.0.0.0/api',
+    baseURL: 'http://localhost:8000/api',
 });
+
+export const $authHost = axios.create({
+    baseURL: 'http://localhost:8000/api',
+});
+
+$authHost.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('access_token');
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
