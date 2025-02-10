a938d6fd20b180dd492bf36f5ff60639baf0c7be
olov2020 <vladimir.8624@mail.ru>
2025-02-10 11:46:52 +0300

    fix docker

--- finka_back/finka/settings.py
+++ finka_back/finka/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5bu8ef*9@k##m(wey6ktly1#k-w^i$^j+d5c*-%b81+rbcm0j='
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
