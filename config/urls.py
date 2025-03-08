f205ca07910bf63c3f5227b81100c0b48966c9de
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 22:28:17 +0300

    updated urls

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('users/', include('users.urls')),
-    path('transactions/', include('transactions.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/transactions/', include('transactions.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
