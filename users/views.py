885809eca4dadb05dba90d6be92bc3602811feca
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 23:23:18 +0300

    fixed urls and view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
-from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import UserSerializer
 
-User = get_user_model()
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "message": "User registered successfully",
+            "user": {
+                "id": user.id,
+                "email": user.email,
+            }
+        }, status=status.HTTP_201_CREATED)
