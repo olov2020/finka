7fa844715c6e4cde82855a2cc7b2bad38192530b
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 23:23:18 +0300

    fixed urls and view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'', UserViewSet)
+from django.urls import path
+from .views import UserRegistrationView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
 ]
