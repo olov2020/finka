f286f42ac0acfe1222e92971f076a7e2cdf3fc3c
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 23:20:39 +0300

    fixed serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+User = get_user_model() # Получаем модель пользователя
 
+# Сериализатор для пользователя
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
 
+    # Мета класс для указания модели и полей
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'is_active', 'is_staff')
-        read_only_fields = ('id', 'is_active', 'is_staff')
+        fields = ('id', 'email', 'password')
+        read_only_fields = ('id')
 
+    # Метод для создания пользователя
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = User(**validated_data)
@@ -18,6 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    # Метод для обновления пользователя
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
         for attr, value in validated_data.items():
