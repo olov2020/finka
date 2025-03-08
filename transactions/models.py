8100ee262622dca300e183d71ab6b74ed945e62b
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 22:19:48 +0300

    created transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    category = models.CharField(
+        max_length=100
+    )
+
+    def __str__(self):
+        return f"{self.date} - {self.category} - {self.amount} руб."
