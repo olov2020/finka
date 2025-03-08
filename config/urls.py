2b3c74fc2ddf869fdd62099ea82366de6d62ce53
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-08 22:18:08 +0300

    added transaction api into urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include('users.urls')),
+    path('transactions/', include('transactions.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
