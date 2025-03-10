20cf94ea7dae1a7c90cdbacc03038b648641bf15
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-10 23:26:14 +0300

    add url for profile

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import UserRegistrationView
 from .views import CustomTokenObtainPairView
+from .views import UserProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
 ]
