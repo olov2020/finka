618e21656094fdf48803afaf428a6f235d657e57
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-10 23:47:03 +0300

    add view for password change

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
-from .serializers import UserSerializer
-from .serializers import CustomTokenObtainPairSerializer
-from .serializers import UserProfileSerializer
+from .serializers import UserSerializer, ChangePasswordSerializer, UserProfileSerializer, CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 # Представление регистрации пользователя
 class UserRegistrationView(generics.CreateAPIView):
@@ -36,3 +36,13 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+# Представление изменения пароля пользователя
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "Пароль успешно изменен."}, status=status.HTTP_200_OK)
