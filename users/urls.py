8df9250c82c654e00eb0c264a22da8bc9d7f0814
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-10 22:57:06 +0300

    add url for auth

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import UserRegistrationView
+from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
