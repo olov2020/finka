5432c69a3c64ec916def66d11b928da1b9a707e9
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-11 00:19:48 +0300

    created crud api

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import UserRegistrationView, CustomTokenObtainPairView, UserProfileView, ChangePasswordView
+from django.urls import path, include
+from .views import UserRegistrationView, CustomTokenObtainPairView, UserProfileView, ChangePasswordView, UserViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'', UserViewSet, basename='user')  # Роутер для CRUD операций
 
 urlpatterns = [
+    path('crud/', include(router.urls)),
+
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
