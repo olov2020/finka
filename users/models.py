c40c8be0cc2ec5b6c777ee17418cc7f9011b3e0f
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-10 22:54:28 +0300

    changed model, so the all field are seen by serializator

--- users/models.py
+++ users/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 # Класс для управления пользователями
 class UserManager(BaseUserManager): 
-    def create_user(self, email, password=None): # Метод для создания пользователя
+    def create_user(self, email, password=None, **extra_fields): # Метод для создания пользователя
         if not email: # Проверяем что email был передан
             raise ValueError("Email must be set")
         
         email = self.normalize_email(email) # Приводим email в корректный вид
-        user = self.model(email=email) # Создаем экземпляр пользователя
+        user = self.model(email=email, **extra_fields) # Создаем экземпляр пользователя
         user.set_password(password) # Устанавливаем пароль в хэшированном виде
         user.save(using=self._db) # Сохраняем пользователя в базу данных
         return user
@@ -29,16 +29,16 @@ class UserManager(BaseUserManager):
 # Класс пользователя
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True, verbose_name="Email")
-    phone = models.CharField(max_length=16, unique=True, verbose_name="Phone number")
-    first_name = models.CharField(max_length=64, verbose_name="First name")
-    last_name = models.CharField(max_length=64, verbose_name="Last name")
+    phone = models.CharField(max_length=16, unique=True, verbose_name="Phone number", default="")
+    first_name = models.CharField(max_length=64, verbose_name="First name", default="")
+    last_name = models.CharField(max_length=64, verbose_name="Last name", default="")
     date_of_birth = models.DateField(verbose_name="Date of birth", blank=True, null=True)
     password = models.CharField(max_length=128, verbose_name="Password")
 
     objects = UserManager() # Устанавливаем через что создается пользователь
 
     USERNAME_FIELD = 'email' # Меняем логин на email
-    REQUIRED_FIELDS = [first_name, last_name, date_of_birth] # Поля которые обязательны для заполнения, в данном случае не указываем потому что email - обязательное поле
+    REQUIRED_FIELDS = ['phone', 'first_name', 'last_name', 'date_of_birth'] # Поля которые обязательны для заполнения, в данном случае не указываем потому что email - обязательное поле
 
     def __str__(self): # Метод для отображения пользователя в админке
         return self.email
