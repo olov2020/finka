6c54131123abedf48903acaa9491b6153275c657
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-10 21:32:09 +0300

    fixed serializer

--- users/serializers.py
+++ users/serializers.py
@@ -10,23 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
     # Мета класс для указания модели и полей
     class Meta:
         model = User
-        fields = ('id', 'email', 'password')
-        read_only_fields = ['id']
+        fields = ['id', 'email', 'phone', 'first_name', 'last_name', 'date_of_birth', 'password']
 
     # Метод для создания пользователя
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            phone=validated_data['phone'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            date_of_birth=validated_data['date_of_birth'],
+            password=validated_data['password']
+        )
         return user
-
-    # Метод для обновления пользователя
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        if password:
-            instance.set_password(password)
-        instance.save()
-        return instance
