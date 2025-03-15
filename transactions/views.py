3b9444ce89a72aec49f198dcc65d7cfd46af6828
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-03-15 19:35:22 +0300

    changed views

--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,21 @@
-from rest_framework import generics
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Transaction
 from .serializers import TransactionSerializer
+from .filters import TransactionFilter
 
-class TransactionCreateView(generics.CreateAPIView):
-    queryset = Transaction.objects.all()
+class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TransactionFilter
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Transaction.objects.all()
+        return Transaction.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
