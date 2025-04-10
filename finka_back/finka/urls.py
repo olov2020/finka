43104274f2bb273b31897753fd25f90b62b20ed0
olov2020 <vladimir.8624@mail.ru>
2025-04-10 22:16:56 +0300

    delete false back

--- finka_back/finka/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # update token
-    path('api/register/', RegistrationView.as_view(), name='register'), # registration
-]
