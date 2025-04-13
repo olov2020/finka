384b6fd19028015fe976875e3ddec025e7fd7aa2
Kostalevsky-Daniil <daniilkostalevsky@yandex.ru>
2025-04-14 00:24:23 +0300

    delete old files

--- transactions/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Transaction
-
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = ['id', 'user', 'name', 'amount', 'sum', 'currency', 'category', 'date', 'type', 'comment']
-        read_only_fields = ['id', 'user']
